4496e7425dd751b5c39142b23c2ed93c6a2cf000
vanminh0311 <85997835+vanminh0311@users.noreply.github.com>
2022-04-21 05:51:57 +0700

    Initial commit

---
+++ Scripts/AutoBuild_DiyScript.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# AutoBuild Module by Hyy2001 <https://github.com/Hyy2001X/AutoBuild-Actions>
+# AutoBuild DiyScript
+
+Firmware_Diy_Core() {
+
+	Author=AUTO
+	Author_URL=AUTO
+	Default_FLAG=AUTO
+	Default_IP="192.168.1.1"
+	Banner_Message="Powered by AutoBuild-Actions"
+
+	Short_Firmware_Date=true
+	Checkout_Virtual_Images=false
+	Firmware_Format=AUTO
+	REGEX_Skip_Checkout="packages|buildinfo|sha256sums|manifest|kernel|rootfs|factory"
+
+	INCLUDE_AutoBuild_Features=true
+	INCLUDE_Original_OpenWrt_Compatible=false
+}
+
+Firmware_Diy() {
+
+	# 请在该函数内定制固件
+
+	# 可用预设变量, 其他可用变量请参考运行日志
+	# ${OP_AUTHOR}			OpenWrt 源码作者
+	# ${OP_REPO}			OpenWrt 仓库名称
+	# ${OP_BRANCH}			OpenWrt 源码分支
+	# ${TARGET_PROFILE}		设备名称
+	# ${TARGET_BOARD}		设备架构
+	# ${TARGET_FLAG}		固件名称后缀
+
+	# ${Home}				OpenWrt 源码位置
+	# ${CONFIG_FILE}		使用的配置文件名称
+	# ${FEEDS_CONF}			OpenWrt 源码目录下的 feeds.conf.default 文件
+	# ${CustomFiles}		仓库中的 /CustomFiles 绝对路径
+	# ${Scripts}			仓库中的 /Scripts 绝对路径
+	# ${FEEDS_LUCI}			OpenWrt 源码目录下的 package/feeds/luci 目录
+	# ${FEEDS_PKG}			OpenWrt 源码目录下的 package/feeds/packages 目录
+	# ${BASE_FILES}			OpenWrt 源码目录下的 package/base-files/files 目录
+
+	:
+}
